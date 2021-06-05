3848804c0f4a6cec7b169469a8df86e6c253ef90
jbped <pedigojacob@gmail.com>
2021-06-04 23:27:10 -0600

    first iteration of api trending calls and horizontal scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,10 +32,32 @@ var saveConfig = function(response) {
 }
 
 // Get Trending Movie window value (day || week)
-var trendWindow = function() {
-    var trendVal = $("#trend-window").children(".active").attr("trend-id") + "?"
+var trendWindow = function(clickedBtn) {
+    var startingVal = $("#trend-window").children(".active").attr("trend-id") + "?"
+    if (!clickedBtn) {
+        trendVal = startingVal;
+    }
+    else if (clickedBtn === "Daily") {
+        trendVal = "day?"
+    } 
+    else if (clickedBtn === "Weekly") {
+        trendVal = "week?"
+    }
+
+    if (trendVal === "day?") {
+        $("#trending-title").text("Films Trending Today");
+    } else {
+        $("#trending-title").text("Films Trending this Week");
+    }
+    console.log(trendVal)
+
     getTrend(trendVal);
 }
+
+$("#trend-window").on("click", function(target){
+    var clickedBtn = $(target)[0].target.innerText
+    trendWindow(clickedBtn);
+})
 
 // Call trending films 
 var getTrend = function(trendVal) {
@@ -60,16 +82,42 @@ var getTrend = function(trendVal) {
 
 // Render trending films
 var renderTrend = function(response) {
-    for (var i = 0; i < response.results.length; i++){
-        var genDiv = $("<div></div>");
-            genDiv.addClass("film-card");
-        var movTitle = document.createElement(h5);
-            movTitle.text(response.results[i].title);
-            genDiv.append(movTitle);
+    trendContentCont.html("");
+    for (var i = 0; i < 10; i++){
+        var genCard = $("<div></div>");
+            genCard.addClass("card bg-dark text-light film-card mx-3 my-2 w-25");
+            genCard.css("width", "10rem");
+            
         var postImg = $("<img></img>");
-            postImg.attr("src", configJson.images.base_url + )
+            postImg.attr("src", imgUrl + postSizCust + response.results[i].poster_path);
+            postImg.addClass("card-img-top");
+        genCard.append(postImg);
+        var imgOverlay = $("<div></div>");
+            imgOverlay.addClass("card-img-overlay");
+        var cardBody = $("<div></div>");
+            cardBody.addClass("card-body");
+            // cardBody.css("word-wrap", "break-word");
+        var movTitle = $("<h5></h5>");
+            movTitle.text(response.results[i].title);
+            movTitle.addClass("card-title w-100");
+            // movTitle.css("word-wrap", "break-word");
+        if (i > 3) {
+            genCard.addClass("media-hide");
+        }
+            cardBody.append(movTitle);
+        // imgOverlay.append(movTitle);
+        genCard.append(cardBody);
+        // genCard.append(imgOverlay);
+        trendContentCont.append(genCard);
     }
 }
 
 configurationApi();
+
+var imgUrl = configJson.images.base_url;
+var postSizCust = "w220_and_h330_face"
+var postSize185 = configJson.images.poster_sizes[2];
+var postSize342 = configJson.images.poster_sizes[3];
+var postSize500 = configJson.images.poster_sizes[4];
+
 trendWindow();

641f84946c849ff898771adab101388526184279
jbped <pedigojacob@gmail.com>
2021-06-07 20:04:18 -0600

    Additional comments, adding attributes to rendered items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,3 @@
-// // TMDB API Information
-// var tmdbUrl = "https://api.themoviedb.org/3/";
-// var tmdbKey = "&api_key=2c8ad8ff8fd528fe53a66ae9ef906e6b";
-// var getTrending = "trending/movie/"
-// var topRated = "movie/top_rated"
-// var popFilms = "movie/popular"
-// var showingFilms = "movie/now_playing"
-// var popPeople = "person/popular"
-// var genreSearch = "genre/movie/list"
-// var multiSearch = "/search/multi?query="
-
-// // movieGlu API Information
-// var gluKey = "api-key=Dm6PtlSwNc4Vjt4WhaSTS1dXGqu9Vu48gz9TqwN0";
-// var gluUrl = "https://api-gate2.movieglu.com/";
-
-// // Global Variables/DOM elements
-// var searchBar = $("#basic-search");
-// var trendContentCont = $("#trending-content-cont");
-// var topRatedCont = $("#rated-content-cont");
-// var popFilmsCont = $("#pop-content-cont");
-// var inTheatersCont = $("#showing-content-cont");
-// var popPeopleCont = $("#pop-people-content-cont");
-// var genreCont = $("#genre-content-cont");
-// GEOLOCATION ------ START
-// var options = {enableHighAccuracy: true, timeout: 5000, maximumAge: 0}
-
-// function success(pos) {
-//     var crd = pos.coords;
-//     var lat = crd.latitude;
-//     var lon = crd.longitude;
-
-//     console.log(`Your current position is`);
-//     console.log(`Latitude: ${lat}`);
-//     console.log(`Longitude: ${lon}`);
-// }
-// function error(err)
-// {
-//     console.warn(`ERROR(${err.code}): ${err.message}`);
-// }
-
-// navigator.geolocation.getCurrentPosition(success, error, options);
-// GEOLOCATION ------ END
-
-// var theatersNearMe = function() {
-//     locationTheaters = JSON.parse(localStorage.getItem("locationTheaters"));
-
-//     if (!locationTheaters) {
-//         fetch (
-//             gluKey + "cinemasNearby/?n=5" + gluUrl
-//         )
-//         .then (function(response){
-//             if(response.ok) {
-//                 return response.json();
-//             }
-//         })
-//         .then (function(response){
-//             saveTheatersNearMe(response);
-//         })
-//     }
-// }
-// var saveTheatersNearMe = function(response) {
-//     localStorage.setItem("locationTheaters", JSON.stringify(response));
-// }
-
-// // Load or call TMDB Configuration Api
-// var configurationApi = function() {
-//     configJson = JSON.parse(localStorage.getItem("configJson"));
-
-//     if (!configJson) {
-//         fetch (
-//             tmdbUrl + "configuration?" + tmdbKey
-//         )
-//         .then (function(response){
-//             if(response.ok) {
-//                 return response.json();
-//             }
-//         })
-//         .then (function(response){
-//             saveConfig(response);
-//         })
-//     }
-// }
-
-// // Save config api call
-// var saveConfig = function(response) {
-//     localStorage.setItem("configJson", JSON.stringify(response));
-// }
-
-// function openNav() {
-//     document.getElementById("mySidenav").style.width = "250px";
-//   }
-  
-//   /* Set the width of the side navigation to 0 */
-//   function closeNav() {
-//     document.getElementById("mySidenav").style.width = "0";
-//   }
-
-
-
 // --------------------------------------------------------------------------------------
 // HOME PAGE API LOGIC FOR DISPLAYING MOVIE INFO ON LOAD
 // --------------------------------------------------------------------------------------
@@ -263,7 +164,6 @@ var getGenres = function() {
 //--------------------------------------------------------------------------------------
 // RENDER HOME PAGE API CALLS
 //--------------------------------------------------------------------------------------
-
 // Render trending films (getTrend())
 var renderTrend = function(response) {
     trendContentCont.html("");
@@ -282,6 +182,8 @@ var renderTrend = function(response) {
         var movTitle = $("<h5></h5>");
             movTitle.text(response.results[i].title);
             movTitle.addClass("card-title w-100");
+            movTitle.attr("content-type",response.results[i].media_type);
+            movTitle.attr("content-id",response.results[i].id);
         if (i > 3) {
             genCard.addClass("media-hide");
         }
@@ -309,6 +211,8 @@ var renderTopRated = function(response) {
         var movTitle = $("<h5></h5>");
             movTitle.text(response.results[i].title);
             movTitle.addClass("card-title w-100");
+            movTitle.attr("content-type",response.results[i].media_type);
+            movTitle.attr("content-id",response.results[i].id);
         if (i > 3) {
             genCard.addClass("media-hide");
         }
@@ -336,6 +240,8 @@ var renderPopFilms = function(response) {
         var movTitle = $("<h5></h5>");
             movTitle.text(response.results[i].title);
             movTitle.addClass("card-title w-100");
+            movTitle.attr("content-type",response.results[i].media_type);
+            movTitle.attr("content-id",response.results[i].id);
         if (i > 3) {
             genCard.addClass("media-hide");
         }
@@ -363,6 +269,8 @@ var renderShowingFilms = function(response) {
         var movTitle = $("<h5></h5>");
             movTitle.text(response.results[i].title);
             movTitle.addClass("card-title w-100");
+            movTitle.attr("content-type",response.results[i].media_type);
+            movTitle.attr("content-id",response.results[i].id);
         if (i > 3) {
             genCard.addClass("media-hide");
         }
@@ -390,6 +298,8 @@ var renderPopPeople = function(response) {
         var movTitle = $("<h5></h5>");
             movTitle.text(response.results[i].name);
             movTitle.addClass("card-title w-100");
+            movTitle.attr("content-type","person");
+            movTitle.attr("content-id",response.results[i].id);
         if (i > 3) {
             genCard.addClass("media-hide");
         }
@@ -424,17 +334,9 @@ var renderGenre = function(response) {
         genreCont.append(genCard);
     }
 }
-
-// configurationApi();
-// theatersNearMe();
-
-// var imgUrl = configJson.images.base_url;
-// var postSizCust = "w220_and_h330_face"
-// var postSize185 = configJson.images.poster_sizes[2];
-// var postSize342 = configJson.images.poster_sizes[3];
-// var postSize500 = configJson.images.poster_sizes[4];
-// var profilePicSiz = "w235_and_h235_face"
-
+// --------------------------------------------------------------------------------------
+// Functions Called on Load
+// --------------------------------------------------------------------------------------
 trendWindow();
 getTopRated();
 getPopFilms();
--- assets/js/trending.js
+++ assets/js/trending.js
@@ -3,21 +3,25 @@ var windowTitle = $("#trend-window-title");
 var typeTitle = $("#trend-media-title");
 console.log(contentType)
 
+// Default page for api pagination
 var trendingPage = 1;
 
+// Update Media Type Button Classes to display Active status
 $("#media-type-group").on("click", function(target){
     var clickedBtn = $(target)[0].target;
     $(clickedBtn).addClass("active");
     $(clickedBtn).siblings().removeClass("active");
 })
 
+// Logic of Trending Option Aside Menu
 var trendSettings = function() {
     $(trendContentCont).html("")
     trendingPage = 1;
     trendType = $("#media-type-group").children(".active").attr("trend-type");
     trendWindow = $("#trend-window").children(".active").attr("trend-id");
     console.log("Trend Media Type:",trendType,'\n',"Trend Media Window:",trendWindow);
-
+    
+    // Dynamic title to correspond with displayed content
     if (trendWindow === "day") {
         $(windowTitle).text("Daily");
     } else if (trendWindow === "week") {
@@ -39,6 +43,8 @@ var trendSettings = function() {
     fetchTrending(trendType, trendWindow);
 
 }
+
+// Initial Trending on Page Load
 var initTrending = function() {
     var trendType = "all";
     var trendWindow = "day";
@@ -48,6 +54,7 @@ var initTrending = function() {
     fetchTrending(trendType, trendWindow);
 }
 
+// Trending Api Call
 var fetchTrending = function(a, b) {
     fetch (
         tmdbUrl + getTrending + a + "/" + b + "?page=" + trendingPage + tmdbKey
@@ -62,6 +69,7 @@ var fetchTrending = function(a, b) {
     })
 }
     
+// Rending of Trending Call
 var renderTrending = function (response) {
     for (var i = 0; i < response.results.length; i++) {
         var genCard = $("<div></div>");
@@ -74,7 +82,7 @@ var renderTrending = function (response) {
             var movTitle = $("<h5></h5>");
             movTitle.text(response.results[i].title);
             movTitle.addClass("card-title w-100");
-            movTitle.attr("content-type","movie");
+            movTitle.attr("content-type",response.results[i].media_type);
             postImg.attr("src", imgUrl + postSizCust + response.results[i].poster_path);
             postImg.addClass("card-img-top");
         }
@@ -82,7 +90,7 @@ var renderTrending = function (response) {
             var movTitle = $("<h5></h5>");
             movTitle.text(response.results[i].name);
             movTitle.addClass("card-title w-100");
-            movTitle.attr("content-type","tv");
+            movTitle.attr("content-type",response.results[i].media_type);
             postImg.attr("src", imgUrl + postSizCust + response.results[i].poster_path);
             postImg.addClass("card-img-top");
         }  else {
@@ -100,6 +108,7 @@ var renderTrending = function (response) {
     }
 }
 
+// Load more button logic
 $("#load-more").on("click", function(event) {
     event.preventDefault();
     trendType = $("#media-type-group").children(".active").attr("trend-type");
@@ -108,4 +117,5 @@ $("#load-more").on("click", function(event) {
     fetchTrending(trendType, trendWindow);
 })
 
+// Function Calls on load
 initTrending();
